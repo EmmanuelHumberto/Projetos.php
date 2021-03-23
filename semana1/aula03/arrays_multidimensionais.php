41ac7e701ab240ed459ab6d47697b0b745d8675a
ss <setrotec@outlook.com>
2021-03-23 13:47:40 -0300

    Tenstanto a tipagem dinamica do php

--- semana1/aula03/arrays_multidimensionais.php
+++ semana1/aula03/arrays_multidimensionais.php
@@ -10,15 +10,23 @@
             'Março'     => 29
         ],
         '2030' => [
-            'janeiro'   => 30,
-            'fevereiro' => 32,
-            'Março'     => 29
+            'janeiro'   => true,
+            'fevereiro' => true,
+            'Março'     => false
         ],
         '2040' => [
-            'janeiro'   => 30,
-            'fevereiro' => 32,
-            'Março'     => 29
+            'janeiro'   => "frio",
+            'fevereiro' => "quente",
+            'Março'     => 'fresco'
         ]
     ];  
 
+    // Nesse caso é passado todas as dimensões do array pois se trata de uma matrix 
+    //$temperatura['2020']['janeiro']
     echo "janeiro de 2020 foi de {$temperatura['2020']['janeiro']} graus<br>";
+
+    echo '<hr>';
+
+    echo '<pre>';
+
+var_dump($temperatura);
