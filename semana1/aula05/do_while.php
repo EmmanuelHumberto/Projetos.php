7a58417ccdac53851b7d0c17f50c871ba6231eb2
ss <setrotec@outlook.com>
2021-03-24 20:57:55 -0300

    Estudando estrutura de repetoção do while no php7

--- semana1/aula05/do_while.php
+++ semana1/aula05/do_while.php
@@ -1,1 +1,21 @@
 <?php
+
+$contador = 1;
+
+do{
+
+    $quadrado = $contador * $contador ;
+
+    echo "O quadrado de $contador é $quadrado<br>";
+
+    $contador++;
+
+}while($contador <= 10);
+    
+   
+   
+   
+   
+
+    
+
